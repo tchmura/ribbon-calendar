24adb21de9521d966d9b9cfc982bd71f75b20604
Tomas Chmura <tomas.chmura1@gmail.com>
2020-09-28 15:13:46 +0200

    Setup material UI and styled components for SSR

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import type { AppProps } from 'next/app'
 import { MuiPickersUtilsProvider } from '@material-ui/pickers'
 import MomentUtils from '@date-io/moment'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles)
+    }
+  }, [])
+
   return (
     <MuiPickersUtilsProvider utils={MomentUtils}>
       <Component {...pageProps} />
